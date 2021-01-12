b0b830d8e82108b51c823aeb033ae4d3537dbbdc
mcoulier <michielcoulier@gmail.com>
2021-01-12 09:52:55 +0100

    Users can now log in & register

--- routes/routes.js
+++ routes/routes.js
@@ -4,7 +4,38 @@ const User = require('../models/user');
 const router = require('express').Router();
 
 router.get('/', function (req, res) {
-    res.render('index', {title: "hello", message: "is pug working?"})
+    res.render('index', {user: req.user});
+});
+
+router.get('/register', function (req, res) {
+    res.render('register', {});
+});
+
+router.post('/register', function(req, res, next) {
+    console.log('registering user');
+    User.register(new User({username: req.body.username, email: req.body.email}), req.body.password, function(err) {
+        if (err) {
+            console.log('error while user register!', err);
+            return next(err);
+        }
+
+        console.log('user registered!');
+
+        res.redirect('/');
+    });
+});
+
+router.get('/login', function(req, res) {
+    res.render('login', {user: req.user, /*message: req.flash('error')*/});
+});
+
+router.post('/login', passport.authenticate('local', { failureRedirect: '/login'/*, failureFlash: true */}), function(req, res) {
+    res.redirect('/');
+});
+
+router.get('/logout', function(req, res) {
+    req.logout();
+    res.redirect('/');
 });
 
 module.exports = router;
